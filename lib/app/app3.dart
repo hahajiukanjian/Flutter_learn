b3d4c60690c54add4e2a769668c38f30cdec33cb
hahajiukanjian <hahajiukanjian@outlook.com>
2024-09-04 11:26:08 +0900

    图片荧光

--- lib/app/app3.dart
+++ lib/app/app3.dart
@@ -12,17 +12,43 @@ class App extends StatelessWidget {
         alignment: Alignment.topCenter,
         width: 350,
         height: 350,
-        decoration: const BoxDecoration(
-          color: Colors.deepOrange,
-          gradient: LinearGradient(
-            colors: [
-              Colors.yellow,
-              Colors.blue,
-              Colors.purpleAccent,
-              Colors.cyan,
-            ],
-          ),
-        ),
+        decoration: BoxDecoration(
+            color: Colors.deepOrange,
+            gradient: const LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              colors: [
+                Colors.yellow,
+                Colors.blue,
+                Colors.purpleAccent,
+                Colors.cyan,
+              ],
+            ),
+            image: const DecorationImage(
+              image: NetworkImage(
+                'https://resources.ninghao.net/images/IMG_2626.JPG',
+              ),
+              fit: BoxFit.cover,
+              colorFilter: ColorFilter.mode(
+                Colors.blue,
+                BlendMode.softLight,
+              ),
+            ),
+            borderRadius: const BorderRadius.all(
+              Radius.circular(35),
+            ),
+            border: Border.all(
+              color: Colors.blue,
+              width: 5,
+              style: BorderStyle.solid,
+            ),
+            boxShadow: const [
+              BoxShadow(
+                color: Colors.blue,
+                offset: Offset(5, 20),
+                blurRadius: 30,
+              ),
+            ]),
         child: Image.asset(
           'assets/images/logo.png',
           width: 96,
